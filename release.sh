e728f0e685c90daa0fd485536ea48068d6afbfb0
llambrecht <louis.lambrecht@chargemap.com>
2024-07-06 19:04:39 +0200

    feat: improve release script

--- release.sh
+++ release.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Define your addon name and version
+# Define your addon name
 ADDON_NAME="websites_hidder_for_firefox"
-ADDON_VERSION="1.0.0"
+
+# Ask the user for the version number
+echo "Enter the version number for the release:"
+read ADDON_VERSION
 
 # Directory where your addon source code resides
 SRC_DIR="src"
@@ -15,9 +18,9 @@ rm -rf "$RELEASE_DIR"
 mkdir -p "$RELEASE_DIR"
 
 # Copy necessary files and directories to release directory
-cp -r "$SRC_DIR" "$RELEASE_DIR"
-cp -r manifest.json "$RELEASE_DIR"
-cp -r background.js "$RELEASE_DIR"
+cp -r "$SRC_DIR"/* "$RELEASE_DIR"
+cp manifest.json "$RELEASE_DIR"
+cp background.js "$RELEASE_DIR"
 cp -r assets "$RELEASE_DIR"  # Example: if you have an assets directory
 
 # Exclude unwanted files and directories
@@ -25,5 +28,8 @@ cd "$RELEASE_DIR"
 rm -f README.md CONTRIBUTING.md package.json package-lock.json .gitignore
 rm -rf node_modules
 
+# Create a zip file of the release directory
+cd ..
+zip -r "${ADDON_NAME}-${ADDON_VERSION}.zip" "$RELEASE_DIR"/*
 
 echo "Release package created: ${ADDON_NAME}-${ADDON_VERSION}.zip"
